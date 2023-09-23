072881298ad670080863d38513c68b8a117fd6d2
eLNeTTa <ignacio.delgado.403@gmail.com>
2023-09-23 18:36:24 -0300

    commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\csanm\OneDrive\Desktop\BOP\venv
+set VIRTUAL_ENV=C:\Users\ignac\OneDrive\Documentos\BOP\BOP\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
