4fc3ca0c80b0828149ee30042c7473ec9c509e83
Carlangasss <carloskimax@gmail.com>
2023-09-24 19:03:29 -0300

    creacion de tablas

--- crud/admin.py
+++ crud/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Dispositivo
 from .models import Usuario
+from .models import Comuna
+from .models import Provincia
+from .models import Region
+from .models import Propiedad
+from .models import Sectores
+
 # Register your models here.
 admin.site.register(Dispositivo)
 admin.site.register(Usuario)
+admin.site.register(Comuna)
+admin.site.register(Provincia)
+admin.site.register(Region)
+admin.site.register(Propiedad)
+admin.site.register(Sectores)
--- crud/models.py
+++ crud/models.py
@@ -3,11 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Dispositivo(models.Model):
-    nombreDisp = models.CharField(max_length=200, null=False, unique=True, verbose_name= "Nombre Dispositivo")
+    titulo = models.CharField(max_length=200, null=False, unique=True, verbose_name= "Nombre Dispositivo")
     cordenadaX = models.CharField(max_length=100)
     cordenadaY = models.CharField(max_length=100)
     consumoHora = models.CharField(max_length=100, verbose_name= "Consumo por hora")
-    fecha = models.DateTimeField(max_length=50,verbose_name="Fecha")
 
     def __str__(self):
         return self.titulo
@@ -19,3 +18,47 @@ class Usuario(models.Model):
 
     def __str__(self):
         return self.nombreUsuario
+    
+class Comuna(models.Model):
+    idComuna = models.CharField(primary_key=True, unique=True,verbose_name="ID",max_length=30)
+    nombreComuna = models.CharField(max_length=200, null=False, verbose_name="Comuna")
+    
+    def __str__(self):
+        return self.nombreComuna
+    
+class Provincia(models.Model):
+    idProvincia = models.CharField(primary_key=True, unique=True, verbose_name="ID",max_length=30)
+    nombreProvincia = models.CharField(max_length=200, null=False, verbose_name="Provincia")
+
+    def __str__(self):
+        return self.nombreProvincia
+    
+class Region(models.Model):
+    idRegion = models.CharField(primary_key=True, unique=True, verbose_name="ID",max_length=30)
+    nombreRegion = models.CharField(max_length=200, null=False, verbose_name="Región")
+
+    def __str__(self):
+        return self.nombreRegion
+    
+class Propiedad(models.Model):
+    idPropiedad = models.CharField(primary_key=True, unique=True, verbose_name="ID",max_length=30)
+    nombrePropiedad = models.CharField(max_length=200, null=False, verbose_name="Nombre de la Propiedad")
+    direccionPropiedad = models.CharField(max_length=200, null=False, verbose_name="Dirección")
+
+    def __str__(self):
+        return self.nombrePropiedad
+
+class Sectores(models.Model):
+    idSector = models.CharField(primary_key=True, unique=True, verbose_name="ID",max_length=30)
+    nombreSector = models.CharField(max_length=200, null=False, verbose_name="Nombre del Sector")
+    metrosCuadrados = models.FloatField(max_length=15, verbose_name="Metros^2")
+
+    def __str__(self):
+        return self.nombreSector
+    
+class TipoSector(models.Model):
+    idSector = models.CharField(primary_key=True, unique=True, verbose_name="ID",max_length=30)
+    tipoSector = models.CharField(max_length=200, null=False, verbose_name="Tipo Sector")
+
+    def __str__(self):
+        return self.tipoSector
